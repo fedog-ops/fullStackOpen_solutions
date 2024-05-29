b39352d2a3a2a6fdf9e3f2dc7ac03af111b6dd9e
Felix McDermott <felixmcdermott@Felixs-MacBook-Pro.local>
2024-05-29 20:05:22 +0100

    2.15 Phonebook step 10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
 import Filter from '../src/components/Filter'
+import Notification from './components/Notification'
 import PersonForm from '../src/components/PersonForm'
 import Persons from '../src/components/Persons'
 import { useState, useEffect } from 'react'
@@ -9,6 +10,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFiler] = useState('')
+  const [message, setMessage] = useState({text: null, type: 'prompt'})
 
   const handleNoteChange = (event) => {
   setNewName(event.target.value)
@@ -32,10 +34,51 @@ const handleNumChange = (event) => {
       .remove(id)
       .then(response=>{
         setPersons(persons.filter(p => p.id !== id))
+        resetFormFields();
+        showMessage(`User deleted.`, 'prompt');
       })
+      .catch(error => {
+        showMessage(`User already deleted`, 'error');
+      });
     }
   }
 
+  const resetFormFields = () => {
+    setNewName('');
+    setNewNumber('');
+};
+
+  const showMessage = (text, type) => {
+    setMessage({ text, type });
+    setTimeout(() => setMessage({ text: null, type: 'prompt' }), 5000);
+  };
+
+  const updatePerson = (id, personsObject) => {
+    personService
+      .update(id, personsObject)
+      .then(returnedPerson => {
+        setPersons(persons.filter(person => person.id !== id).concat(returnedPerson));
+        resetFormFields();
+        showMessage(`${returnedPerson.name}'s number was updated.`, 'prompt');
+      })
+      .catch(error => {
+        showMessage(`Information of ${personsObject.name} has already been removed from server`, 'error');
+        setPersons(persons.filter(p => p.id !== id));
+      });
+  };
+
+  const addNewPerson = (personsObject) => {
+    personService
+      .create(personsObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson));
+        resetFormFields();
+        showMessage(`${returnedPerson.name}'s number was added.`, 'prompt');
+      })
+      .catch(error => {
+        showMessage(`Failed to add ${personsObject.name}'s number.`, 'error');
+      });
+  };
 
   const addName = (event) => {
       event.preventDefault()
@@ -43,27 +86,35 @@ const handleNumChange = (event) => {
       const personsObject ={
         name: newName,
         number: newNumber,
+      } 
+      
+      if (!newName) {
+        alert('Please enter a name')
+        return
       }
-    if (persons.some(m => m.name === personsObject.name)) alert(`${newName} is already added to phonebook`)
-    if (!newName) alert('Please enter a name')
-    else personService
-            .create(personsObject)
-            .then(returnedPerson => {
-              console.log(returnedPerson) 
-              // db will assaign Object a new id
-              setPersons(persons.concat(returnedPerson))
-              setNewName('')
-              setNewNumber('')
-            })
+  
+      if (persons.some(m => m.name === personsObject.name)) {
+        const confirmUpdate = window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`);
+        
+        if (confirmUpdate) {
+         const id = persons.find(item => item.name === newName).id;
+         updatePerson(id, personsObject);
+        }
+       
+      }
+       else addNewPerson(personsObject);
   }
   const personsAfterFilter = 
     filter === ''  ? persons : persons.filter(person => 
       person.name.toLowerCase().includes(filter.toLowerCase()))
-  
+   
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification
+      className={message.type}
+      message={message.text}/>
       
       <Filter params={{filter, setFiler}}/>
       <PersonForm 
